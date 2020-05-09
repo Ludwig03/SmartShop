285ea2bc711787c1014023537cff46ab25f8a87b
Ludwig <64146475+Ludwig03@users.noreply.github.com>
2020-05-09 04:10:29 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -216,3 +216,18 @@ $(window).on('load', function() {
 
 
 })(jQuery);
+
+window.onload = function(){
+  var paginationPage = parseInt($('.cdp').attr('actpage'), 10);
+  $('.cdp_i').on('click', function(){
+    var go = $(this).attr('href').replace('#!', '');
+    if (go === '+1') {
+      paginationPage++;
+    } else if (go === '-1') {
+      paginationPage--;
+    }else{
+      paginationPage = parseInt(go, 10);
+    }
+    $('.cdp').attr('actpage', paginationPage);
+  });
+};
